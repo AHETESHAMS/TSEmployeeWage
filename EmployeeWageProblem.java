a503d5bd418f0b8e2c14a887fc3c0d7e35e612a5
AHETESHAMS <aheteshams007@gmail.com>
2023-12-28 11:01:31 +0530

    Added: UC2 Calculate Daily Employee Wage

--- EmployeeWageProblem.java
+++ EmployeeWageProblem.java
@@ -2,5 +2,6 @@ class EmplotyeeWageProblem {
 	public static void main(String []args) {
 		System.out.println("Welcome to Employee Wage");
 		System.out.println("Added UC1");
+		System.out.println("Added UC2");
 	}
 }
