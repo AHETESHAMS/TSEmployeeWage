3d9c7d0edd93f795deeba1fd0c176bd204cd3849
AHETESHAMS <aheteshams007@gmail.com>
2023-12-28 10:53:29 +0530

    Added: UC 1 Checking Employee is Present or Absent

--- EmployeeWageProblem.java
+++ EmployeeWageProblem.java
@@ -1,5 +1,6 @@
 class EmplotyeeWageProblem {
 	public static void main(String []args) {
-		System.out.println("Welcome to Employee Wage")
+		System.out.println("Welcome to Employee Wage");
+		System.out.println("Added UC1");
 	}
 }
